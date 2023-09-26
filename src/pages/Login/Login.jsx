1fa01a7942b50b199e109028ebc097ecd9655bb3
tomasu <tomas.broz1998@gmail.com>
2023-09-26 14:38:45 +0100

    hashed password security check

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,36 +1,28 @@
 import classes from './Login.module.scss'
 import { useDispatch, useSelector } from 'react-redux';
 import { login } from '../../store/userSlice.js';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
+import bcrypt from 'bcryptjs'
 
 export default function Login() {
-    const [username, setUsername] = useState();
-    const [password, setPassword] = useState();
+    const username = useRef();
+    const password = useRef();
 
     const user = useSelector(state => state.user);
     const dispatch = useDispatch();
 
-    const handleUsernameInput = (e) => {
-        setUsername(e.target.value);
-    }
-
-    const handlePasswordInput = (e) => {
-        setPassword(e.target.value);
-    }
-
     const handleLogin = () => {
         dispatch(login({
-            name: username, 
-            password: password,
-            loggedIn: true
+            name: username.current.value ?? null, 
+            password: password.current.value ?? null
         }));
     }
 
     return (
         <form onSubmit={ (e) => e.preventDefault() } className={ classes.login_form }>
             <h1>Login</h1>
-            <input onChange={ handleUsernameInput } type="text" placeholder="Username" />
-            <input onChange={ handlePasswordInput } type="password" placeholder="Password" />
+            <input ref={ username } type="text" placeholder="Username" />
+            <input ref={ password } type="password" placeholder="Password" />
             <button onClick={ handleLogin }>Login</button>
             {user.error ? <div className={ classes.error }>{ user.error }</div> : null}
         </form>
--- src/store/userSlice.js
+++ src/store/userSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import bcrypt from 'bcryptjs';
 
 export const userSlice = createSlice({
     name: "user",
@@ -8,8 +9,18 @@ export const userSlice = createSlice({
     },
     reducers: {
         login: (state, action) => {
-            
-            if(process.env.REACT_APP_USERNAME === action.payload.name && process.env.REACT_APP_PASSWORD === action.payload.password) {
+            if(!action.payload.name){
+                state.error = "Username is required";
+                return;
+            }
+            if(!action.payload.password){
+                state.error = "Password is required";
+                return;
+            }
+
+            const isPasswordValid = bcrypt.compareSync(action.payload.password, process.env.REACT_APP_PASSWORD);
+
+            if(process.env.REACT_APP_USERNAME === action.payload.name && isPasswordValid) {
                 state.user = action.payload;
                 state.error = null;
                 localStorage.setItem('user', state.user.name);
